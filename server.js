f1d59808fb239e0ee53a32e56483a573feaebdd0
tapos <tapos.aa@gmail.com>
2017-04-05 00:53:50 +0600

    rest api mathod writing done

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/students',(req,res)=>{
+   res.send("get all method call");
+});
+
+app.post('/students',(req,res)=>{
+    res.send("post method  call");
+});
+
+app.get('/students/:studentId',(req,res)=>{
+    res.send("get a single student  method call");
+});
+app.put('/students/:studentId',(req,res)=>{
+    res.send("get a single student update  method call");
+});
+app.del('/students/:studentId',(req,res)=>{
+    res.send("get a single student delete method call");
+});
+
+app.listen(3000,()=>{
+    console.log(`connected successfully in port`);
+});
